8fa653afec32b5416ab299670de47228251b4079
SUNDARPRASAD M <sundarprasad22@gmail.com>
2024-01-03 22:55:41 +0530

    git added initial file

---
+++ src/test/java/com/prasad/captoneprojectecommerce/CaptoneProjectEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasad.captoneprojectecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptoneProjectEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
